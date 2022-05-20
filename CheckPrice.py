67c3121c3cca594059149240b38552af6a35925b
frmee <105950004+frmee@users.noreply.github.com>
2022-05-21 04:42:39 +0700

    Update CheckPrice.py

--- CheckPrice.py
+++ CheckPrice.py
@@ -40,8 +40,8 @@ def notify(symbol,frequency):
         print(notification)
         print('\n')
         send_discord(url,notification)
-        # chỉ thông báo mỗi 1 phút 1 lần
-        time.sleep(60)
+        # chỉ thông báo mỗi 2 phút 1 lần
+        time.sleep(120)
 
 notify(symbols, frequency)
        
